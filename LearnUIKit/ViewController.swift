350bea1cea49da01f64f15b5786003e2730b7dfa
Sothesom <151612168+sotheso@users.noreply.github.com>
2024-07-10 16:00:18 -0700

    Initial Commit

---
+++ LearnUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnUIKit
+//
+//  Created by Sothesom on 20/04/1403.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
